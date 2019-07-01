2eea70fa0feca91aaec26bafd97576d350f32371
wangsin <wang_sin@163.com>
2019-07-02 00:40:22 +0800

    Add SQL file.

---
+++ db.sql
@@ -0,0 +1,107 @@
+create table Customer
+(
+    id      int      not null,
+    name    char(64) not null,
+    tel     char(32) not null,
+    company char(64) not null,
+    constraint Customer_id_uindex
+        unique (id),
+    constraint Customer_tel_uindex
+        unique (tel)
+);
+
+alter table Customer
+    add primary key (id);
+
+create table Material
+(
+    id   int      not null,
+    name char(32) not null,
+    rest int      not null,
+    constraint Material_id_uindex
+        unique (id),
+    constraint Material_name_uindex
+        unique (name)
+);
+
+alter table Material
+    add primary key (id);
+
+create table `Order`
+(
+    order_id      int                  not null,
+    start_date    date                 not null,
+    end_date      date                 not null,
+    custom_id     int                  null,
+    is_available  tinyint(1) default 0 not null,
+    is_finished   tinyint(1) default 0 not null,
+    is_urgent     tinyint(1) default 0 not null,
+    need_material int        default 0 not null,
+    need_stock    int        default 6 not null,
+    constraint Order_order_id_uindex
+        unique (order_id),
+    constraint Order_Customer_id_fk
+        foreign key (custom_id) references Customer (id)
+            on update cascade on delete cascade
+);
+
+alter table `Order`
+    add primary key (order_id);
+
+create table Job
+(
+    id              int       not null,
+    order_id        int       not null,
+    input_path      char(64)  not null,
+    best_time       int       not null,
+    best_aps        char(128) not null,
+    best_solution   char(128) not null,
+    result_img_path char(128) not null,
+    constraint Job_id_uindex
+        unique (id),
+    constraint Job_Order_order_id_fk
+        foreign key (order_id) references `Order` (order_id)
+            on update cascade on delete cascade
+);
+
+alter table Job
+    add primary key (id);
+
+create table Stock
+(
+    room_id    int not null,
+    rest_num   int not null,
+    full_num   int not null,
+    last_order int not null,
+    constraint Stock_room_id_uindex
+        unique (room_id),
+    constraint Stock_Order_order_id_fk
+        foreign key (last_order) references `Order` (order_id)
+            on update cascade on delete cascade
+);
+
+alter table Stock
+    add primary key (room_id);
+
+create table User
+(
+    name             char(64)             not null,
+    id               int                  not null,
+    password         char(128)            not null,
+    email            char(64)             not null,
+    simulate_num     int                  null,
+    last_login       datetime             null,
+    confirmed        tinyint(1) default 0 not null,
+    is_administrator tinyint(1) default 0 not null,
+    constraint User_Email_uindex
+        unique (email),
+    constraint User_Id_uindex
+        unique (id),
+    constraint User_Name_uindex
+        unique (name)
+);
+
+alter table User
+    add primary key (id);
+
+
